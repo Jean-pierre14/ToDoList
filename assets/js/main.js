935dc9e1b0ea6603ef3054001b9c76d589ab7c88
Jean-pierre14 <chiruzabisimwa@gmail.com>
2022-06-14 14:02:13 +0200

    form

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+const open = document.getElementById('addBtn'),
+    close = document.getElementById('close'),
+    content = document.getElementById('content-box')
+
+// content.style.display = 'none';
+
+// Open modal
+open.onclick = () => {
+    // alert("Open")
+
+    content.style.display = 'block';
+
+}
+
+// close modal
+close.onclick = () => {
+    // alert("Close")
+
+    content.style.display = 'none';
+
+}
