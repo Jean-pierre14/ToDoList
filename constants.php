64a1163e1f86489429a1773ddd4222df2c8da5b0
Jean-pierre14 <chiruzabisimwa@gmail.com>
2022-06-20 08:29:31 +0200

    Constants classes

--- constants.php
+++ constants.php
@@ -1,11 +1,16 @@
 <?php
 
 class ConstantsClass{
+
     const Message = "Grace bisimwa";
+    
     public function Bye(){
+    
         echo self::Message;
+    
     }
 }
 
 $go = new ConstantsClass();
+
 $go->Bye();
